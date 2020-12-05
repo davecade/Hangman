5dcb00211afde6028748547831204fa9fc40b87c
Dave <dave.cadelina@outlook.com>
2020-12-05 23:47:24 +1100

    created home.js IFFE

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+const Home = (()=> {
+    
+    const init = () => {
+        render();
+        listeners();
+    }
+
+    const listeners = () => {
+        
+    }
+
+    const render = () => {
+
+    }
+
+    return {
+        init
+    }
+})();
+
+export default Home;
