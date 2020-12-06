847529b2003eb1e49ebe377833aef5bd7512b91e
Dave <dave.cadelina@outlook.com>
2020-12-06 15:29:17 +1100

    basic hangman letter checking and display completed

--- js/modules/game.js
+++ js/modules/game.js
@@ -5,38 +5,73 @@ const Game = (()=> {
 
     //-- Variables
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const words = ['apple', 'cat', 'headphones', 'sanitizer']
+    let lives = 10;
+    let answer
+    let guessingWord
 
     // -- Initialize
     const init = () => {
+        answer = words[Math.floor(Math.random()*words.length)].split("")
+        guessingWord = Array(answer.length).fill('_')
         render();
         listeners();
     }
 
     // -- State
     const state = {
-        selectedLetters: []
+        selectedLetters: [],
     }
 
     // -- Event Listeners
     const listeners = () => {
+        $hangman.addEventListener('click', event => {
+            if(event.target.parentNode.matches('.hangman-letters')) {
+                check(event.target.innerHTML.toLowerCase())
+            }
+        })
+    }
 
+    const alreadyTaken = letter => {
+        return state.selectedLetters.includes(letter)
+    }
+
+    const updateGuessingWord = guess => {
+        answer.forEach((letter, index) => {
+            if(letter===guess.toLowerCase()) {
+                guessingWord[index] = guess.toLowerCase()
+            }
+        })
     }
 
     const createLetters = () => {
         let markup = ''
         alphabet.forEach(letter => {
-            markup += `<li ${state.selectedLetters.includes(letter) ? "class='selected'" : ""}>${letter.toUpperCase()}</li>`
+            markup += `<li ${alreadyTaken(letter) ? "class='selected'" : ""}>${letter.toUpperCase()}</li>`
         })
         return markup
     }
 
+    const check = guess => {
+        if(alreadyTaken(guess)) return;
+        state.selectedLetters.push(guess)
+
+        if(answer.includes(guess)) {
+            updateGuessingWord(guess)
+        } else {
+            lives--;
+        }
+        
+        render();
+    }
+
     const render = () => {
         let markup = `
-            <p class="lives">Lives: 0</p>
+            <p class="lives">Lives: ${lives}</p>
             <div class="game">
                 <h1 class="title">Hangman</h1>
                 <canvas class="hangman-board" height="155px"></canvas>
-                <div class="guessing-word">____</div>
+                <div class="guessing-word">${guessingWord.join("")}</div>
                 <p>Pick a letter below to guess the whole word.</p>
                 <ul class="hangman-letters">
                     ${createLetters()}
@@ -44,7 +79,6 @@ const Game = (()=> {
                 <button class="btn">Main Menu</button>
             </div>
         `
-        console.log(state.selectedLetters)
         $hangman.innerHTML = markup;
     }
 
