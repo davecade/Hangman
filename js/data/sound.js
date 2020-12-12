0f05af1f6fdca33556f9bf722b9590e873abf574
Dave <dave.cadelina@outlook.com>
2020-12-12 16:49:40 +1100

    changed src location for audio files to the github raw src file

--- js/data/sound.js
+++ js/data/sound.js
@@ -1,5 +1,7 @@
 export const sound = {
-  win: new Audio('../media/win.mp3'),
-  lose: new Audio('../media/lose.mp3'),
-  click: new Audio('../media/click.mp3')
+    win: new Audio('https://raw.githubusercontent.com/davecade/Hangman/main/media/win.mp3'),
+    lose: new Audio('https://raw.githubusercontent.com/davecade/Hangman/main/media/lose.mp3'),
+    click: new Audio('https://raw.githubusercontent.com/davecade/Hangman/main/media/click.mp3')
 }
+
+
